2589dfdc7d2b17f87f01189c9a493077d8e56919
Ed Emeruwa <emeruwaed@gmail.com>
2025-04-14 00:32:25 -0700

    frontend fix 5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Home from "../src/components/Home.jsx";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import AddTask from "./components/addTask.jsx";
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard.jsx";
 import CompletedTasks from "./components/completedTasks.jsx";
 import Profile from "./components/profile.jsx";
 import AssignedTasks from "./components/assignedTasks.jsx"; // The AssignedTasks component
